82e9fc24063a6840545dbea572719eafe00ed6b1
Keaghon <keaghonc@gmail.com>
2025-10-21 21:22:07 -0500

    feat: Added buttons to the popup instead of on the task card.

--- todoApp/static/tasks/scripts.js
+++ todoApp/static/tasks/scripts.js
@@ -1,8 +1,15 @@
 const modal = document.getElementById("taskModal");
+const editButton = document.getElementById("editButton");
+const deleteButton = document.getElementById("deleteButton");
+
 function openModal(el) {
     document.getElementById("modalTitle").textContent = el.dataset.title;
     document.getElementById("modalDescription").textContent = el.dataset.description;
     document.getElementById("modalDue").textContent = el.dataset.dueDate || "No due date";
+
+    editButton.href = el.dataset.edit_url;
+    deleteButton.href = el.dataset.delete_url;
+
     modal.style.display = "block";
 }
 
